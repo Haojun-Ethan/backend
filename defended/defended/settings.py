3f041234def7ab1c92cffb24c446267d29ed2d1f
Haojun-Ethan <haojun.jia@uon.edu.au>
2026-09-24 15:24:34 +1000

    backend from Deepad, test 009

--- defended/defended/settings.py
+++ defended/defended/settings.py
@@ -37,7 +37,11 @@ DATABASE_URL = os.environ.get('DATABASE_URL')
 print("DEBUG DATABASE_URL:", repr(DATABASE_URL))
 if DATABASE_URL:
     DATABASES = {
-        'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600)
+        'default': dj_database_url.config(
+            default=DATABASE_URL,
+            conn_max_age=600,
+            conn_health_checks=True,
+        )
     }
 else:
     DATABASES = {
